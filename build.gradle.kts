4971482163af679f61ce2b5389f6b8db217120e7
rickchoijd <rick.choi@justice.gov.uk>
2026-09-23 18:39:08 +0100

    ESWE-2155: Update dependencies
    Update plugin:
    - `uk.gov.justice.hmpps.gradle-spring-boot` `11.0.9`
    - Kotlin `2.4.20`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.8"
-  kotlin("plugin.spring") version "2.4.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "11.0.9"
+  kotlin("plugin.spring") version "2.4.20"
 }
 
 configurations {
